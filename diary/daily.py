141a570b8d5aff66df5ec308fa1adf151a27b211
AlloJS <eventorganization1@gmail.com>
2024-08-02 08:37:44 +0200

    Testing

--- diary/daily.py
+++ diary/daily.py
@@ -1,11 +1,12 @@
 import datetime
 import time
 import calendar
-
+from dateutil import tz
 def _daily_note(year,month,day,description,do):
-
-    d_d = datetime.date(year,month,day)
-    date_diary = datetime.date.strftime(d_d,'%Y-%m-%d')
+    time_zone = tz.tzutc()
+    #region = tz.gettz('Europe/Paris')
+    d_d = datetime.datetime(year,month,day,tzinfo=time_zone)
+    date_diary = datetime.datetime.strftime(d_d,'%Y-%m-%d %H:%M:%S')
     obj_diary = {'date':date_diary,'description':description,'do':do,'calendar':calendar.month(year,month)}
     return obj_diary
 
@@ -20,7 +21,14 @@ def get_daily_diary(year,month,day,description,do):
         :return: Return a string of daily diary
     '''
     obj_daily = _daily_note(year, month, day, description, do)
-    str_daily = 'Month of:\n' + obj_daily['calendar'] + '\n' + obj_daily['date'] + ':\n' + obj_daily['description'] + '\n' + 'Do:' + str(obj_daily['do'])
+    str_daily = (f"Month of:\n{obj_daily['calendar']}\n"
+                 f"{obj_daily['date']}:\n{obj_daily['description']}\n"
+                 f"Do: {obj_daily['do']}")
+    
     return str_daily
+
+
+print(get_daily_diary(2024,9,10,'Compleanno Angelo',False))
+
 
 
